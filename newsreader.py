340622a951a08b5baef6c287e154f4c2bc8af33a
Kaushal Dhungel <57295839+Kaushal-Dhungel@users.noreply.github.com>
2019-11-07 16:32:40 +0530

    added some extra comments
    extra comments nothing other

--- newsreader.py
+++ newsreader.py
@@ -9,17 +9,19 @@ def speak(str):
 
 if __name__ == '__main__':
     speak("Hello, welcome to newstoday.com. I am your news anchor")
-    speak(" top news for today from India and World are ")
+    speak(" top news for today from  are ")
     print("Hello, welcome to newstoday.com. I am your news anchor")
     print(" top news for today from India and World are ")
-
+    
+    # You can use any of the given api.
+    
     #google top ten headlines
     #url = ('https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=301dde8d4fd841e097ffeac8ed52d953') #fetch the url
 
     #top news from USA
     #url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=301dde8d4fd841e097ffeac8ed52d953')
 
-    #top news from  India
+    #top news from  India and world. I found this api better than other.
     url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=301dde8d4fd841e097ffeac8ed52d953')
 
 
@@ -35,7 +37,7 @@ if __name__ == '__main__':
       #  print("next news")
 
     for i in range (1,16):   #only limited no of news..generates from 1 to 15
-        item = f"news no {i}" # i have used the concept of f strings learned from 'Harry'..feels great
+        item = f"news no {i}" # i have used the concept of f strings 
         print(item)
         speak(item)  # speak funcn takes only string so i converted changing val of 'i' to string and passed here.
         print(article[i]['title']) #same as 'for i in articles: print i'
